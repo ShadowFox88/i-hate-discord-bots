028187d4ff964f234f19de58fa634156b614e38c
cyrus01337 <cyrus21337@gmail.com>
2024-05-05 23:42:04 +0000

    Add pre-commits and a bunch of extensions lol

---
+++ .devcontainer/install-dependencies.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env
+arguments=()
+
+if [ -f requirements.dev.txt ]; then
+    arguments+="-r requirements.dev.txt "
+fi
+
+if [ -f requirements.txt ]; then
+    arguments+="-r requirements.txt"
+fi
+
+pip install $arguments
+pre-commit autoupdate
+pre-commit install
