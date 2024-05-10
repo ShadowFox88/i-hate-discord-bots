1878a54bda22d7f153fb4aa79fff8ad84905f384
cyrus01337 <cyrus21337@gmail.com>
2024-05-10 13:10:29 +0000

    Remove archive

--- archive/install-dive.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-DIVE_VERSION=$(curl -sL "https://api.github.com/repos/wagoodman/dive/releases/latest" | grep '"tag_name":' | sed -E 's/.*"v([^"]+)".*/\1/')
-
-curl -OL https://github.com/wagoodman/dive/releases/download/v${DIVE_VERSION}/dive_${DIVE_VERSION}_linux_amd64.deb
-apt-get install -y ./dive_${DIVE_VERSION}_linux_amd64.deb
